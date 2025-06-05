47f6f5a937f1e03cae48b1f66763ca8e3783f615
Jetfire679 <42152813+Jetfire679@users.noreply.github.com>
2025-06-05 15:53:24 -0400

    Update stuff.sh

--- stuff.sh
+++ stuff.sh
@@ -1,3 +1,7 @@
 stuff
 
 dig example.com +stats | awk '/^;; QUERY:/ {query=$4} /Query time:/ {print query, $4 " ms"}'
+
+
+domain=example.com; dig $domain +stats | awk -v d="$domain" '/Query time:/ {print d, $4 " ms"}'
+
