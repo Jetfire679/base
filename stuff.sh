1662e581cdbab05a7b8a39face96e9c7cdd87048
Jetfire679 <42152813+Jetfire679@users.noreply.github.com>
2025-06-27 09:20:20 -0400

    Update stuff.sh

--- stuff.sh
+++ stuff.sh
@@ -5,3 +5,12 @@ dig example.com +stats | awk '/^;; QUERY:/ {query=$4} /Query time:/ {print query
 
 domain=example.com; dig $domain +stats | awk -v d="$domain" '/Query time:/ {print d, $4 " ms"}'
 
+
+max_attempts=10
+attempt_num=1
+while [ -f /var/lib/rpm/.rpm.lock ] && [ "$attempt_num" -le "$max_attempts" ]; do
+  echo "Waiting for RPM lock... ($attempt_num)"
+  sleep 5
+  attempt_num=$((attempt_num + 1))
+done
+
